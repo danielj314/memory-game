1373d8bb20230903f683b33454636a5cad39e733
danielj314 <danieljones314@gmail.com>
2022-05-10 22:38:53 +0000

    js code for discovering if cards match or not and what functions to carry out in either situation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,47 @@
-const cards = document.querySelectorAll('.card')
+const cards = document.querySelectorAll('.card');
+
+let hasBeenFlipped = false;
+let firstCard, secondCard;
+
+// Card flip function
 
 function turnOver() {
-    this.classList.toggle('turn');
+    this.classList.add('turn');
+
+    if (!hasBeenFlipped) {
+    // grab first card
+    hasBeenFlipped = true;
+    firstCard= this;
+    } else {
+    // grab second card
+    hasBeenFlipped = false;
+    secondCard= this;
+    
+    checkForMatch()
+    }
+}
+
+    // do cards match?
+function checkForMatch() {
+    if (firstCard.dataset.animal === secondCard.dataset.animal) {
+        keepCardsFlipped()
+    } else {
+        unflipCards()
+    }
+}
+
+// Cards match
+function keepCardsFlipped() {
+    firstCard.removeEventListener('click', turnOver);
+    secondCard.removeEventListener('click', turnOver);
+}
+
+// Cards dont match
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('turn');
+        secondCard.classList.remove('turn');
+        },  1000);
 }
 
 cards.forEach(card => card.addEventListener('click', turnOver))
